c8ed7e29ef96a41e15d91389c9c6314084ec5478
sbhawt@gmail.com <sbhawt@gmail.com>
2023-08-03 17:19:44 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,64 @@
+import Footer from "./components/footer/Footer.tsx";
+import Menu from "./components/menu/Menu.tsx";
+import Navbar from "./components/navbar/Navbar.tsx";
+import Home from "./pages/home/Home.tsx";
+import Login from "./pages/login/Login.tsx";
+import Products from "./pages/products/Products.tsx";
+import Users from "./pages/users/Users.tsx";
+import "./styles/global.scss";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+
 function App() {
 
-  return (
-    <div>Hello World</div>
-  )
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar/>
+          <div className="container">
+            <div className="menuContainer">
+              <Menu/>
+            </div>
+            <div className="contentContainer">
+              <Outlet/>
+            </div>
+          </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/users",
+          element: <Users/>
+        },
+        {
+          path: "/products",
+          element: <Products/>
+        },
+      ]
+    },
+
+    {
+      path:"/login",
+      element: <Login/>
+    }
+  ]);
+
+  return <RouterProvider router={router}/>
 }
 
 export default App
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Admin Dashboard</span>
+      <span>&copy; 27 INC</span>
+    </div>
+  )
+}
+
+export default Footer
